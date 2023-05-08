98599b6012a84ac11b62e8e2d75e77051ee96278
minhtuann <lamtuanvt9x@gmail.com>
2023-05-08 20:30:40 +0300

    communication between login.jsx and login api established

---
+++ frontend/src/pages/api/login.js
@@ -0,0 +1,26 @@
+
+
+export default async function handlerLogin(req, res) {
+    if (req.method === "POST") {
+        // Process a POST request
+        const {email, password} = req.body;
+
+        // CHECK if user exists in database
+        //...
+        // CHECK if the password is correct
+        //...
+        // if user exists and password is correct, then return a success response
+        const response = {
+            status: 'success',
+            message: 'User logged in successfully',
+            data: {
+                email,
+                password,
+            },
+        };
+        res.status(200).json(response);
+    } else {
+        // Handle any other HTTP method
+        res.status(200).json({ name: 'John Doe' })
+    }
+}
--- frontend/src/pages/entry/login.jsx
+++ frontend/src/pages/entry/login.jsx
@@ -1,25 +1,37 @@
-import Link from "next/link"
-import { useState } from "react"
+import Link from "next/link";
+import { useState } from "react";
+import Router from "next/router";
 export default function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
-      e.preventDefault()
-      const response = await fetch("http://localhost:3000/api/login", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-      })
-      const data = await response.json()
-      console.log("loginData:", data)
-  }
-    return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    console.log("loginData:", data);
+
+    if (data.status === "success") {
+      // redirect to home page
+      Router.push("/");
+    } else {
+      if (data.status === "fail") {
+        data.errors.map((err) => {
+          console.log(err.msg);
+        });
+      }
+    }
+  };
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h1 className="mt-10 text-center text-3xl font-american-type-writer leading-9 tracking-tight text-gray-900">
             Irelevant Discussion(s)
           </h1>
@@ -28,75 +40,91 @@ export default function Login() {
             src="../images/My_Video_Page.png"
             alt="Your Company"
           />
-            <h2 className="mt-10 text-center text-2xl font-american-type-writer leading-9 tracking-tight text-gray-900">
-              Sign in
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label htmlFor="email" className="block text-md font-american-type-writer leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-md font-american-type-writer leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-md">
-                    <a href="#" className="font-american-type-writer text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-md font-american-type-writer leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-10 text-center text-md text-gray-500">
-              Not a user?{' '}
-              <Link href="/entry/register" className="font-american-type-writer leading-6 text-indigo-600 hover:text-indigo-500">
-                Register here
-              </Link>
-            </p>
-          </div>
+          <h2 className="mt-10 text-center text-2xl font-american-type-writer leading-9 tracking-tight text-gray-900">
+            Sign in
+          </h2>
         </div>
-      </>
-    )
-  }
-  
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            className="space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={handleSubmit}
+          >
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-md font-american-type-writer leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-md font-american-type-writer leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-md">
+                  <a
+                    href="#"
+                    className="font-american-type-writer text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-md font-american-type-writer leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-md text-gray-500">
+            Not a user?{" "}
+            <Link
+              href="/entry/register"
+              className="font-american-type-writer leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Register here
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
