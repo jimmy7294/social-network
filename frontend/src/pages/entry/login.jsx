202a689ca7e7870dd3398fa4e7c959b68e808bd2
minhtuann <lamtuanvt9x@gmail.com>
2023-05-08 10:32:59 +0300

    frontend login register api

--- frontend/src/pages/entry/login.jsx
+++ frontend/src/pages/entry/login.jsx
@@ -1,8 +1,7 @@
 
-export default function Register() {
+export default function Login() {
     return (
-        <>
-
+      <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
@@ -11,7 +10,7 @@ export default function Register() {
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Register Here Buddy
+              Sign in
             </h2>
           </div>
   
@@ -61,13 +60,15 @@ export default function Register() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Register
+                  Sign in
                 </button>
               </div>
             </form>
   
             <p className="mt-10 text-center text-sm text-gray-500">
+              Not a user?{' '}
               <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                Register here
               </a>
             </p>
           </div>
