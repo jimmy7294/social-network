4284763a3fe550e0967dd7174c8d48d676af4fdb
rasmushaq <rasmus.a.j.haques@gmail.com>
2023-05-08 13:19:14 +0300

    cookiechecker and redirect

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,109 +1,75 @@
-import { headers } from "next/dist/client/components/headers";
-import { useState } from "react";
+// import { headers } from "next/dist/client/components/headers";
+// import { redirect } from "next/dist/server/api-utils";
+// import Link from "next/link";
+// import { useEffect, useState } from "react";
+import Cookies from 'js-cookie'
+import { useRouter } from 'next/router';
 
-// function to build Registration
-function RegistrationComp(){
+function MyComponent() {
+  const router = useRouter();
 
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    nickname: '',
-    about: '',
-    firstName: '',
-    lastName: '',
-
-  });
-//makes sure that the submuit it not empty/sends a fetch to see if username/password is correct
-  const HandleRegistration = async (e) => {
-    //makes sure form is not empty will need changing
-    e.preventDefault();
-
-    const res = await fetch('/registrationvalidation', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (res.ok) {
-     localStorage.setItem
-    } else {
-      // Handle error
-    }
-  };
-  // takes the new changes in the form and updates the formdata
-  const HandleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  //build inside of return <>
-return(
-  <>
-      <div>
-        <h2>FUCKING NAME IT </h2>
-      </div>
-      <div>
-        <h2>mooo</h2>a
-      </div>
-      </>
-)
+  if (Cookies.get('myCookie') !== undefined) {
+    // The cookie exists, do something
+    return(
+    <h2>Hello</h2>
+    )
+  } else {
+    router.push('/entry/login');
+  }
 }
 
 
-// function to build Login window
-function LoginComp(){
-  //updates form values
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-//makes sure that the submuit it not empty/sends a fetch to see if username/password is correct
-  const HandleSubmit = async (e) => {
-    e.preventDefault();
 
-    const res = await fetch('/loginvalidation', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({email,password}),
-    });
+// function Chat(){
+//     return()
+// }
 
-    if (res.ok) {
-      // Handle success
-    } else {
-      // Handle error
-    }
-  };
-  // takes the new changes in the form and updates the formdata
-  const HandleChange = (e) => {
-    setEmail(e.taget.value)
-    setPassword(e.target.value)
-  };
+// function Posts(){
+//     // should be getting all posts
+//     const [posts, SetPosts] = useState([])
+//     useEffect(() => {
+//         const fetchPosts = async () => {
+//           const res = await fetch('/getallposts');
+//           const data = await res.json();
+//           setPosts(data);
+//         };
+      
+//         fetchPosts();
+//       }, []);
+// return()
+// }
+    
 
-  //login component/design
-  return(
-    <>
-    <div>
-      <h2>Something</h2>
-    </div>
-    </>
-  )
-}
+//     return()
+// }
+
+// function Following(){
+//     return()
+// }
+
+// function SearchBar(){g
+// return()
+// }
+
+// function MakePost(){
+//     return()
+// }
+
+// function Profile(){
+//     return()
+// }
+
+// function CommonChat(){
+//     return()
+// }
+// function Notifications(){
+//     return()
+// }
+
+
 
 
 export default function Home() {
-  //dont build in here
-  return (
-    <>
-    <div>
-      <RegistrationComp/>
-      </div>
-      <div>
-        <LoginComp/>
-      </div>
-      </>
-  )
+  MyComponent()
+return(<h1>moo</h1>)
 }
