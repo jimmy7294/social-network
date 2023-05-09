664e06e2ff85fab43ca13f6d3d963caa57c19737
minhtuann <lamtuanvt9x@gmail.com>
2023-05-09 11:01:47 +0300

    merge with main

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,24 +4,72 @@
 // import { useEffect, useState } from "react";
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/router';
+import { headers } from "next/dist/client/components/headers";
+import { useState } from "react";
 
-function MyComponent() {
-  const router = useRouter();
-  if (typeof window !== 'undefined') {
-    if (Cookies.get('myCookie') !== undefined) {
-      // The cookie exists, do something
-      return(
-        <h2>Hello</h2>
-      )
-    } else {
-      router.push('/entry/login');
-    }
-  }
+// function to build Registration
+function RegistrationComp(){
 
 
+
+
+  //build inside of return <>
+return(
+  <>
+      <div>
+        <h2>FUCKING NAME IT </h2>
+      </div>
+      <div>
+        <h2>mooo</h2>
+      </div>
+      </>
+)
 }
 
 
+// function to build Login window
+function LoginComp(){
+  //updates form values
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+//makes sure that the submuit it not empty/sends a fetch to see if username/password is correct
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch('/loginvalidation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email,password}),
+    });
+
+    if (res.ok) {
+      // Handle success
+    } else {
+      router.push('/entry/login');
+    }
+  };
+  // takes the new changes in the form and updates the formdata
+  const HandleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //login component/design
+  return(
+    <>
+    <div>
+      <h2>Something</h2>
+    </div>
+    </>
+  )
+}
+
+
+}
 
 // function Chat(){
 //     return()
