7a8fcf02105e6015cc99ee078c83a367974e0cd0
Rasmushaq <Rasmus.a.j.haques@gmail.com>
2023-05-24 10:59:22 +0300

    added comment

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 
+//runs when ever a file is loaded on the page and checks if the cookie is still valid
+
+
 export async function middleware(req, NextRequest){
     const cookie = req.cookies.get('SessionToken')
-    
+    //if there is no cookie pressent at all, redirct to login page
 if(cookie === undefined){
     return NextResponse.redirect("http://localhost:3000/login")
 }
+// if cookie is pressent sends to backend for validation
 const response = await fetch("http://localhost:8080/api/cookie", {
       method: "POST",
       headers: {
@@ -17,11 +21,11 @@ const response = await fetch("http://localhost:8080/api/cookie", {
     });
     const data = await response.json();
 
-    
+    //if cookie is different from the DB throw error and stay on login
     if (!data.ok) {
       throw new Error("error in cookie check")
     } 
-
+// eveyrthing ok u may continue
     return NextResponse.next()
 }
 
