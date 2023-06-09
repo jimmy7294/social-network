3682f0e1497fc0946ea283ef66a854a3431a1fdd
Rasmushaq <Rasmus.a.j.haques@gmail.com>
2023-06-09 14:06:41 +0300

    profile fetch trying

--- frontend/app/profile/page.jsx
+++ frontend/app/profile/page.jsx
@@ -4,33 +4,68 @@ export const metadata = {
 
 // create a dummy data object
 
-const ProfilePage = (data) => {
-  
-  return (
-    <div>
-      ProfilePage
-      <h1>Username: {data.username}</h1>
-      <h1>Email: {data.email}</h1>
-      <h1>First Name: {data.first_name}</h1>
-      <h1>Last Name: {data.last_name}</h1>
-      <h1>Bio: {data.about_me}</h1>
-      <h1>Birthday: {data.DOB}</h1>
-      <h1>Avatar</h1>
-      <img src={data.avatar} alt="Avatar" />
-      <h1>Followers</h1>
-      <ul>
-        {data.followers.map((follower) => (
-          <li key={follower}>{follower}</li>
-        ))}
-      </ul>
-      <h1>Following</h1>
-      <ul>
-        {data.following.map((following) => (
-          <li key={following}>{following}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+function getProfile() {
+  const response = fetch("http://localhost:8080/api/getUsernames", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })  
+  .then(data => data.json())
+  .then(data => {
+    if (data.status == "success"){
+      console.log(data)
+    } else {
+      console.log(data)
+    }
+    })
 
+
+
+  // const profile = response.json();
+  // console.log("profile:", profile);
+  // if(profile.status == "success"){
+  // console.log("profile data:", profile);
+  // } else {
+  //   console.log("error profile fetch")
+  // }
+  // return profile;
+}
+
+function ProfilePage(){
+const page = getProfile();
+return (console.log(page))
+
+}
+
+  
+//   return (
+//     <div>
+//       ProfilePage
+//       <h1>Username: {data.username}</h1>
+//       <h1>Email: {data.email}</h1>
+//       <h1>First Name: {data.first_name}</h1>
+//       <h1>Last Name: {data.last_name}</h1>
+//       <h1>Bio: {data.about_me}</h1>
+//       <h1>Birthday: {data.DOB}</h1>
+//       <h1>Avatar</h1>
+//       <img src={data.avatar} alt="Avatar" />
+//       <h1>Followers</h1>
+//       <ul>
+//         {data.followers.map((follower) => (
+//           <li key={follower}>{follower}</li>
+//         ))}
+//       </ul>
+//       <h1>Following</h1>
+//       <ul>
+//         {data.following.map((following) => (
+//           <li key={following}>{following}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ProfilePage;
 export default ProfilePage;
