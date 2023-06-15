e8a8511da40be92368b93bf9b8aab1f7f03af6fd
Rasmushaq <Rasmus.a.j.haques@gmail.com>
2023-06-15 10:54:15 +0300

    header fetch

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -1,6 +1,32 @@
 import Link from "next/link";
 
+ function getTinyProfile() {
+  fetch("http://localhost:8080/api/getProfile", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify("voff"),
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      if (data.status !== "success") {
+        console.log("failed to get profile");
+        return;
+      }
+      console.log(data);
+      return data;
+    });
+}
+
+
+
+
+
+
 const Headers = () => {
+  const data = getTinyProfile()
   return (
     <header className="headbar">
       <div className="identity">
@@ -13,10 +39,18 @@ const Headers = () => {
       </div>
         <div className="navigate">
           <Link className="link-up" href="/profile">Profile</Link>
-          <Link className="link-up" href="/profile/group">Group</Link>
+          <Link className="link-up" href="/group">Group</Link>
           <Link className="link-up" href="/chat">Chat</Link>
           <Link className="link-up" href="/post/">Post</Link>
-          <Link className="link-up" href="/post/"><div className="logout">log out</div></Link>
+        </div>
+        <div className="tinyavatar">
+          <img
+            className="avatar"
+            src={data.avatar}
+            />
+        </div>
+        <div className="tinyname">
+          <Link className="link-up" href="/profile">{data.username}</Link>
         </div>
     </header>
   );
