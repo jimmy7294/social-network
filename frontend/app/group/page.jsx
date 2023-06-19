c12c41d4fc2b76d36413130edae3c31defe54f3c
Rasmushaq <Rasmus.a.j.haques@gmail.com>
2023-06-19 14:14:34 +0300

    make group

--- frontend/app/group/page.jsx
+++ frontend/app/group/page.jsx
@@ -2,6 +2,40 @@
 import React, { useEffect, useState } from "react"
 
 
+function MakeGroup(){
+  const [groupname, setGroupname] = useState("");
+  const [groupdescription, setGroupdescription] = useState("");
+
+const handleNewGroup = () => {
+  e.preventDefault();
+  fetch("http://localhost:8080/api/makeGroup", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({groupname, groupdescription}),
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      if (data.status !== "success") {
+        console.log("failed to make group");
+        return;
+      }
+      console.log("group made");
+    });
+};
+  return (
+    <>
+    <form onSubmit={(e) => handleNewGroup(e)}>
+    <input type="text" placeholder="Group Name" onChange={(e) => setGroupname(e.target.value)}></input>
+    <input type="text" placeholder="Group Description" onChange={(e) => setGroupdescription(e.target.value)}></input>
+    <button type="submit">Make Group</button>
+    </form>
+    </>
+  )
+}
+
 function YourGroups(){
   const [groups, setGroups] = useState([]);
   useEffect(() => {
@@ -45,6 +79,7 @@ fetch("http://localhost:8080/api/getGroupnames", {
 function Groups(){
   return (
     <>
+    <MakeGroup/>
     <YourGroups/>
     </>
   )
