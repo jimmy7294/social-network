da5d459642404bed605d48e6b700c27481348b42
Arshee <jones.aaron107@gmail.com>
2023-06-22 10:58:14 +0300

    haha

--- frontend/app/group/[slug]/page.jsx
+++ frontend/app/group/[slug]/page.jsx
@@ -23,12 +23,39 @@ function GetGroupPage(slug){
                     console.log("get group page failed", data.status)
                     return
                 }
-                console.log("get group page success", data)
-                setGroupPage(data)
-            }
-        )
-    }, []);
-    console.log("group page", groupPage)
+                console.log(data)
+                setGroup(data.group)
+            })
+    }, [])
+    return(
+        <>
+        <div className="group">
+            <h2>group</h2>
+            <h2>{group.name}</h2>
+            <p>{group.description}</p>
+            <p>{group.members}</p>
+            <p>{group.events}</p>
+        </div>
+        </>
+    )
+}
+
+
+function GroupPage(slug){
+    return(
+        <>
+        <Headers />
+        {GetGroup(slug)}
+        <div className="groupPage">
+            <h1>group page</h1>
+            {GetGroup(slug)}
+            <MakeEvent />
+            <GetEvents />
+        </div>
+        </>
+    )
+
+
 }
 
 export default GetGroupPage;
