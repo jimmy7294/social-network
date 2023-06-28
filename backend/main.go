61ee135e13ad05f5e7e2215a3304110bb20b8809
longschlonken <crippe-b@hotmail.com>
2023-06-28 15:47:12 +0300

    forgor to update main.go lole

--- backend/main.go
+++ backend/main.go
@@ -50,6 +50,7 @@ func setupApi() {
 	http.HandleFunc("/api/getHeadbar", apiGO.GetHeadBar)
 	http.HandleFunc("/api/addOrRemoveFollow", apiGO.AddOrRemoveFollow)
 	http.HandleFunc("/api/addGroup", apiGO.AddGroup)
+	http.HandleFunc("/api/addEvent", apiGO.AddEvent)
 	http.HandleFunc("/api/getGroupPage", apiGO.GetGroupPage)
 	img := http.FileServer(http.Dir("internal/images"))
 	http.Handle("/images/", http.StripPrefix("/images/", img))
