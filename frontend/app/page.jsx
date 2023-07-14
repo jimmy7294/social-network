d3457a0d46e709d105c5884bb361e69c90fff430
Jimmy <tuan.lam@gritlab.ax>
2023-07-14 19:47:03 +0300

    Posts sorted by date, Filted ok, comments half ok

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -2,72 +2,65 @@
 
 import { useState, useEffect, use } from "react";
 import Headers from "./components/Header";
-import {useRouter} from "next/navigation";
 import encodeImageFile from "./components/encodeImage";
 import GetYourImages from "./components/GetYourImages";
 import ImageSelector from "./components/imageSelector";
 
-const datapromise = GetYourImages()
+const datapromise = GetYourImages();
 
 function HomePage() {
   //const img = await GetYourImages()
-  const img = use(datapromise)
-  console.log("images from top", img)
+  const img = use(datapromise);
+  // console.log("images from top", img);
   return (
     <>
       <Headers />
-      <MakePost userImages={img}/>
+      <MakePost userImages={img} />
       <GetPosts />
     </>
   );
 }
 
-
-
-function MakeComment(post_id){
-
-
-const [allImages, setAllImages] = useState([]);
-  const [image, setImage] = useState("")
+function MakeComment(post_id) {
+  const [allImages, setAllImages] = useState([]);
+  const [image, setImage] = useState("");
   const [content, setContent] = useState("");
   const [showImages, setShowImages] = useState(false);
+  post_id = post_id.post_id;
+  const post_type = "post";
 
-
-  useEffect (() => {
+  useEffect(() => {
     (async () => {
-      const images = await GetYourImages()
-      setAllImages(images)
-    })()
-  },[])
+      const images = await GetYourImages();
+      setAllImages(images);
+    })();
+  }, []);
 
-
-
-  const handleCommentSubmit = async () => {
-  
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
     fetch("http://localhost:8080/api/addComment", {
       method: "POST",
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ post_id, content, image }),
-      })
+      },
+      body: JSON.stringify({ post_id, content, image, post_type }),
+    })
       .then((data) => data.json())
       .then((data) => {
+        console.log("added comment", data);
         if (data.status !== "success") {
           console.log("failed to add comment", data);
           return;
         }
-        console.log("added comment", data);
-      }
-    );
+      });
   };
   return (
     <>
       <div className="makeComment">
-      {image === "" ? (
+        {image === "" ? (
           <h3>no selected image</h3>
-        ):(
+        ) : (
           <img src={image} className="avatar_preview"></img>
         )}
         <form onSubmit={handleCommentSubmit}>
@@ -78,26 +71,25 @@ const [allImages, setAllImages] = useState([]);
             onChange={(e) => setContent(e.target.value)}
           />
           {showImages ? (
-                <>
-                <button onClick={() => setShowImages(false)}>
-                  X
-                </button>
-                <ImageSelector images={allImages} func={setImage}/>
-                </>
-              ):(
-            
-                <button className="" onClick={() => setShowImages(true)}>
-                  Select Image
-                </button>
-              )}
+            <>
+              <button onClick={() => setShowImages(false)}>X</button>
+              <ImageSelector images={allImages} func={setImage} />
+            </>
+          ) : (
+            <button className="" onClick={() => setShowImages(true)}>
+              Select Image
+            </button>
+          )}
           <button type="submit" className="commentCreationButton">
             submit
           </button>
         </form>
         <form onSubmit={encodeImageFile}>
-            <input type="file" id="image" name="image"></input>
-            <button type="submit" className="text">AddImage</button>
-            </form>
+          <input type="file" id="image" name="image"></input>
+          <button type="submit" className="text">
+            AddImage
+          </button>
+        </form>
       </div>
     </>
   );
@@ -134,79 +126,66 @@ function ToggleComments({ post_id }) {
         <div className="commentos">
           {comments.map((dat, index) => (
             <div key={index} className="commenting">
-              
               <div className="commentDate">{dat.created}</div>
-              <a href={`profile/${dat.author}`} >
-              <div className="commentUser">{dat.author}</div>
+              <a href={`profile/${dat.author}`}>
+                <div className="commentUser">{dat.author}</div>
               </a>
               <div className="commentContent">{dat.content}</div>
             </div>
           ))}
-     <MakeComment post_id={post_id}/>
+          <MakeComment post_id={post_id} />
         </div>
-        
       )}
-      {showMore && !comments && <div>no comments
-      <MakeComment post_id={post_id}/>
-        </div>}
-
-      
+      {showMore && !comments && (
+        <div>
+          no comments
+          <MakeComment post_id={post_id} />
+        </div>
+      )}
     </>
   );
 }
 
-
-function MakePost({userImages}) {
+function MakePost({ userImages }) {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [privacy, setPrivacy] = useState("public");
   const [allowed_users, setAllowed] = useState([]);
   const [allImage, setAllImage] = useState(userImages);
-  const[users,setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   const [image, setimage] = useState();
-  const type = "post"
+  const type = "post";
   const [showImages, setShowImages] = useState(false);
-  //const router = useRouter();
-  const [router, setRouter] = useState(useRouter())
-  console.log(allImage, "lsuadhoashd")
-
-/*   useEffect( ()  =>  {
-//    async () => {
-    const images = GetYourImages()
-    setAllImage(images.user_images)
-    console.log("images",images)
-//    }
-  },[]) */
-
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(allowed_users, "fucked")
-console.log(image, {type,privacy,allowed_users,image,content,title})
+    // console.log(allowed_users, "fucked");
+    // console.log(image, { type, privacy, allowed_users, image, content, title });
     const res = await fetch("http://localhost:8080/api/addPost", {
       method: "POST",
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({type,privacy,allowed_users,image,content,title}),
+      body: JSON.stringify({
+        type,
+        privacy,
+        allowed_users,
+        image,
+        content,
+        title,
+      }),
     });
     const data = await res.json();
     if (data.status !== "success") {
-      console.log("failed to make post");
+      // console.log("failed to make post");
       return;
     }
-    console.log("success");
-    location.reload('/')
+    // console.log("success");
+    location.reload("/");
   };
 
-  
-  
-
   const handlePrivacyChange = async (e) => {
-    console.log("fuckedifuck")
-    console.log(e)
     setPrivacy(e);
     if (e === "semi-private") {
       const res = await fetch("http://localhost:8080/api/getUsernames", {
@@ -223,28 +202,30 @@ console.log(image, {type,privacy,allowed_users,image,content,title})
       }
       setUsers(data.users);
     }
-  
-    
   };
   const AddToAllowed = (e) => {
-    console.log(e.target.checked, "lkusdhfksdhafljkhdsajlkfhljsadfhjlkashljkdfhljksadljkflhjkas")
-    if(e.target.checked){
-    setAllowed(allowed_users => [...allowed_users, e.target.value]);
-  } else {
-    setAllowed(oldArray => oldArray.filter(allowed_users => allowed_users !== e.target.value))
-  }
-  }
- 
+    // console.log(
+    //   e.target.checked,
+    //   "lkusdhfksdhafljkhdsajlkfhljsadfhjlkashljkdfhljksadljkflhjkas"
+    // );
+    if (e.target.checked) {
+      setAllowed((allowed_users) => [...allowed_users, e.target.value]);
+    } else {
+      setAllowed((oldArray) =>
+        oldArray.filter((allowed_users) => allowed_users !== e.target.value)
+      );
+    }
+  };
+
   return (
     <>
       <div className="makePost">
         {image === "" ? (
           <h3>no selected image</h3>
-        ):(
+        ) : (
           <img src={image} className="avatar_preview"></img>
         )}
-        <form 
-        onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <input
             className="titleCreation"
             type="text"
@@ -263,27 +244,19 @@ console.log(image, {type,privacy,allowed_users,image,content,title})
           </select>
           {privacy === "semi-private" && (
             <ul>
-            
-          
-              {users.map((user,index) => (
+              {users.map((user, index) => (
                 <li key={index}>
-                
-                <input type="checkbox" key={index} value={user.username} onChange={e => AddToAllowed(e)} />
-                <label>{user.username}</label>
+                  <input
+                    type="checkbox"
+                    key={index}
+                    value={user.username}
+                    onChange={(e) => AddToAllowed(e)}
+                  />
+                  <label>{user.username}</label>
                 </li>
-
               ))}
-
             </ul>
           )}
-
-{/* allImage.map((image,index) => (
-  <div key={index}>
-    <img src={image} alt="image" className="pfp" onClick={() => setimage(image)} />
-  </div>
-
-))   */
-}
           <textarea
             type="text"
             placeholder="content"
@@ -296,44 +269,34 @@ console.log(image, {type,privacy,allowed_users,image,content,title})
           </button>
         </form>
         <div className="imagePoster">
-            <form onSubmit={encodeImageFile}>
+          <form onSubmit={encodeImageFile}>
             <input type="file" id="image" name="image"></input>
-            <button type="submit" className="text">AddImage</button>
-            </form>
-            {showImages ? (
-                <>
-                <button onClick={() => setShowImages(false)}>
-                  X
-                </button>
-                <ImageSelector images={allImage} func={setimage}/>
-                </>
-              ):(
-            
-                <button className="" onClick={() => setShowImages(true)}>
-                  Select Image
-                </button>
-              )}
-            </div>
+            <button type="submit" className="text">
+              AddImage
+            </button>
+          </form>
+          {showImages ? (
+            <>
+              <button onClick={() => setShowImages(false)}>X</button>
+              <ImageSelector images={allImage} func={setimage} />
+            </>
+          ) : (
+            <button className="" onClick={() => setShowImages(true)}>
+              Select Image
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
 }
 
 function GetPosts() {
-  const [public_posts, setPublic_posts] = useState([]);
-  const [semi_privacys, setSemi_privacys] = useState([]);
-  const [privacys, setPrivacys] = useState([]);
-  const [display, setDisplay] = useState({
-    public: true,
-    semi: true,
-    private: true,
-  });
-
-  const [buttonColor, setButtonColor] = useState({
-    public: "#144b56",
-    semi: "#144b56",
-    private: "#144b56",
-  });
+  const [allPosts, setAllPosts] = useState([]);
+  const [selectedPosts, setSelectedPosts] = useState([]);
+  const [publicPost, setPublicPost] = useState([]);
+  const [semiPost, setSemiPost] = useState([]);
+  const [privatePost, setPrivatePost] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/getPosts", {
@@ -347,162 +310,79 @@ function GetPosts() {
       .then((data) => {
         if (data.status !== "success") {
           console.log("failed to get posts");
-        } 
-        console.log(data, "data here")
-        setPublic_posts(data.posts);
-        setSemi_privacys(data.semi_private_posts);
-        setPrivacys(data.private_posts);
+          return;
+        }
+
+        let mergedPosts = [
+          ...data.posts,
+          ...data.semi_private_posts,
+          ...data.private_posts,
+        ];
+        let publicPost = data.posts;
+        let semiPost = data.semi_private_posts;
+        let privatePost = data.private_posts;
+
+        mergedPosts.sort(
+          (a, b) => new Date(b.creation_date) - new Date(a.creation_date)
+        );
+        publicPost.sort(
+          (a, b) => new Date(b.creation_date) - new Date(a.creation_date)
+        );
+        semiPost.sort(
+          (a, b) => new Date(b.creation_date) - new Date(a.creation_date)
+        );
+        privatePost.sort(
+          (a, b) => new Date(b.creation_date) - new Date(a.creation_date)
+        );
+
+        setAllPosts(mergedPosts);
+        setPublicPost(publicPost);
+        setSemiPost(semiPost);
+        setPrivatePost(privatePost);
+        setSelectedPosts(mergedPosts);
       });
   }, []);
 
-
-
   return (
     <>
-      <div className="groupeOfButtons">
+      <div>
         <button
-          className="buttonPublic"
-          style={{ backgroundColor: buttonColor.public }}
           onClick={() => {
-            setDisplay((prevState) => ({
-              ...prevState,
-              public: !prevState.public,
-            }));
-            setButtonColor((prevColor) => ({
-              ...prevColor,
-              public: display.public ? "#4faa92" : "#144b56",
-            }));
+            setSelectedPosts(allPosts);
           }}
         >
-          Public
+          All
         </button>
-
-        <button
-          className="buttonSemi"
-          style={{ backgroundColor: buttonColor.semi }}
-          onClick={() => {
-            setDisplay((prevState) => ({
-              ...prevState,
-              semi: !prevState.semi,
-            }));
-            setButtonColor((prevColor) => ({
-              ...prevColor,
-              semi: display.semi ? "#4faa92" : "#144b56",
-            }));
-          }}
-        >
-          Semi
-        </button>
-
-        <button
-          className="buttonPrivate"
-          style={{ backgroundColor: buttonColor.private }}
-          onClick={() => {
-            setDisplay((prevState) => ({
-              ...prevState,
-              private: !prevState.private,
-            }));
-            setButtonColor((prevColor) => ({
-              ...prevColor,
-              private: display.private ? "#4faa92" : "#144b56",
-            }));
-          }}
-        >
-          Private
-        </button>
+        <button onClick={() => setSelectedPosts(publicPost)}>Public</button>
+        <button onClick={() => setSelectedPosts(semiPost)}>Semi-Private</button>
+        <button onClick={() => setSelectedPosts(privatePost)}>Private</button>
       </div>
       <div className="posts">
-        {display.public && <PublicPosts posts={public_posts}></PublicPosts>}
-        {display.semi && <SemiPosts posts={semi_privacys}></SemiPosts>}
-        {display.private && <PrivatePosts posts={privacys}></PrivatePosts>}
-      </div>
-    </>
-  );
-}
-
-// PublicPosts component
-function PublicPosts({ posts }) {
-  console.log(posts)
-  return (
-    <>
-    <div className="public">
-      <div className="mfposts">
-        {posts.map((post) => (
-          <div key={post.post_id} className="post">
-            <div className="postDate">Public | {post.creation_date}</div>
-          <a className="link-up" href={`profile/${post.author}`}>
-            <div className="postUser">{post.author}</div>
-            </a>
-
-            <div className="postTitle">{post.title}</div>
-            <div className="postContent">{post.content}
-            {post.image !== null && post.image !== "http://localhost:8080/images/default.jpeg" && post.image !== "" && <img src={post.image} alt="image" className="postImage" />
-            }
-            </div>
-            <ToggleComments post_id={post.post_id}></ToggleComments>
-          </div>
+        {selectedPosts.map((post) => (
+          <Post key={post.post_id} post={post}></Post>
         ))}
-         
       </div>
-    </div>
     </>
   );
 }
 
-// SemiPosts component
-function SemiPosts({ posts }) {
+function Post({ post }) {
   return (
-    <div className="semi">
-      <div className="mfsemi">
-        {posts &&
-          posts.map((semi, index) => (
-            <div key={index} className="post">
-              <div className="postDate">
-                Semi-Private | {semi.creation_date}
-              </div>
-              <a className="link-up" href = {`profile/${semi.author}`}>
-              <div className="postUser">{semi.author}</div>
-              </a>
-              <div className="postTitle">{semi.title}</div>
-              <div className="postContent">{semi.content}
-              {semi.image !== null && semi.image !== "http://localhost:8080/images/default.jpeg" && semi.image !== "" && <img src={semi.image} alt="image" className="postImage" />
-            }
-              </div>
-              <ToggleComments post_id={semi.post_id}></ToggleComments>
-            </div>
-          ))}
+    <div className="post">
+      <div className="postDate">
+        {post.privacy} | {post.creation_date}
       </div>
-    </div>
-  );
-}
-
-
-
-
-
-// PrivatePosts component
-function PrivatePosts({ posts }) {
-  return (
-    <div className="private">
-      <div className="mfprivate">
-        {posts &&
-          posts.map((privacy, index) => (
-            <div key={index} className="post">
-              <div className="postDate">
-                Private | {privacy.creation_date}
-              </div>
-              <a className="link-up" href={`profile/${privacy.author}`}>
-              <div className="postUser">{privacy.author}</div>
-              </a>
-              <div className="postTitle">{privacy.title}</div>
-              <div className="postContent">{privacy.content}
-              {privacy.image !== null && privacy.image !== "http://localhost:8080/images/default.jpeg" && privacy.image !== "" && <img src={privacy.image} alt="image" className="postImage" />
-            }
-              </div>
-              <ToggleComments post_id={privacy.post_id}></ToggleComments>
-            </div>
-          ))}
-      </div>
+      <a className="link-up" href={`profile/${post.author}`}>
+        <div className="postUser">{post.author}</div>
+      </a>
+      <div className="postTitle">{post.title}</div>
+      <div className="postContent">{post.content}</div>
+      {post.image !== null &&
+        post.image !== "http://localhost:8080/images/default.jpeg" &&
+        post.image !== "" && (
+          <img src={post.image} alt="image" className="postImage" />
+        )}
+      <ToggleComments post_id={post.post_id}></ToggleComments>
     </div>
   );
 }
