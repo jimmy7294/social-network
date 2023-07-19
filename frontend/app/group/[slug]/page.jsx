9950e4a3fecb7729bfea501a0ca2b8dac08b3f5c
Rasmushaq <Rasmus.a.j.haques@gmail.com>
2023-07-19 15:35:47 +0300

    event response

--- frontend/app/group/[slug]/page.jsx
+++ frontend/app/group/[slug]/page.jsx
@@ -10,6 +10,25 @@ import { useState, useEffect, use } from "react";
 import ImageSelector from "@/app/components/imageSelector";
 import Link from 'next/link';
 
+
+function Going(data){
+    let going = 0
+    let notGoing = 0
+    for(let i = 0; i < data.length; i++){
+        if(data.anwser === 1){
+            going++
+        }else {
+            notGoing++
+        }
+}
+return(
+    <>
+    <p>Going: {going}</p>
+    <p>Not Going: {notGoing}</p>
+    </>
+)
+}
+
 function InviteToGroup(props) {
   const slug = props.slug
   const members = props.members
@@ -43,10 +62,13 @@ function InviteToGroup(props) {
             return;
           }
           console.log("got usernames", data);
+         
           setAllUsers(data.users.filter(filterOutMembers));
+          
         });
     })();
   }, []);
+  console.log("all users", allusers);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const receiver = username;
@@ -224,7 +246,7 @@ function RenderGroup(props) {
   const [userType, setUserType] = useState("");
   const [isMember, setIsMember] = useState(false);
   const groupname = decodeURIComponent(slug.params.slug);
-
+  
   useEffect(() => {
     if (data !== "not_a_member") {
       setGroupPosts(data.group_posts);
@@ -238,7 +260,29 @@ function RenderGroup(props) {
       setIsMember(false);
     }
   }, []);
+  const HandleEventResponse = async (answer, event_id) => {
+    const group_name = groupname;
+    fetch("http://localhost:8080/api/handleEventAnswer", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({group_name, answer, event_id}),
+        })
+        .then((data) => data.json())
+        .then((data) => {
+            if (data.status !== "success") {
+                console.log("failed to handle event answer", data.status);
+                return;
+            }
+            console.log("handled event answer", data.status);
+        });
+    }
 
+
+
+console.log(events, "events")
   return (
     <>
       <div>
@@ -314,8 +358,17 @@ function RenderGroup(props) {
                           <p>{event.date}</p>
                           <p>{event.event_date}</p>
                         </div>
+                        {!event.already_chosen &&
+                        <form>
+                        <button onClick={() => {HandleEventResponse("going",event.event_id)}}>Going</button>
+                        <button onClick={() => {HandleEventResponse("notgoing", event.event_id)}}>Not Going</button>
+                    </form>
+                                }
+                                {event.already_chosen &&
+                                Going(event.answers)}
                       </div>
                     </div>
+                   
                   </div>
                 ))}
               </div>
@@ -648,167 +701,6 @@ function RequestToJoin(slug) {
     });
 }
 
-/* function GetGroupPage({slug}){
-    console.log("sllluuugggg",slug)
-    const groupname = decodeURIComponent(slug.params.slug)
-  const [groupPosts, setGroupPosts] = useState([])
-  const [events, setEvents] = useState([])
-  const [members , setMembers] = useState([])
-    const [joinRequest, setJoinRequest] = useState([])
-    const [userType, setUserType] = useState("")
-
-    useEffect(() => {
-        fetch("http://localhost:8080/api/getGroupPage", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-                },  
-                body: JSON.stringify(groupname),
-            })
-            .then((data) => data.json())
-            .then((data) => {
-             if (data.status === "success"){
-                setGroupPosts(data.group_posts)
-                setEvents(data.events)
-                setMembers(data.members)
-                setJoinRequest(data.join_request)
-                setUserType(data.member_type)
-                console.log(data)
-             } else {
-                setUserType("not_a_member")
-             }
-            })
-    
-    }, []);
-    if(userType === "not_a_member"){
-        return(
-            <>
-            <h2>{groupname}</h2>
-            <button onClick={RequestToJoin(slug)}>Request to join</button>
-        </>
-
-        )
-    }
-    return(
-        <>
-        <div className="groupPage">
-            <h1>{groupname}</h1>
-           
-            {groupPosts &&
-            <div className="container">
-            {groupPosts.map((post,index) => (
-                <div className="groupPost" key={index}>
-                    
-                    <div className="postti">
-                        <div className="poster">
-                    <img className="pfp" src={post.image} alt="post image" />
-                    <h2>{post.author}</h2>
-                        </div>
-                        <div className="contents">
-                    <p>{post.content}</p>
-                    <p>{post.creation_date}</p>
-                        </div>
-                        <MakeComment post_id={post.post_id}></MakeComment>
-                        <ToggleComments post_id={post.post_id}></ToggleComments>
-                    </div>
-
-                </div>
-                    
-            ))}
-            </div>
-            }
-            {members.username&& 
-            <div>
-                {members.username && (members.username.length == 1) &&
-              
-                <div>
-                    <h1>Members</h1>
-                    <p>{members.username}</p>
-                </div>
-} 
-
-            
-            {members.map((member,index) => (
-                <div className="groupMember" key={index}>
-                    
-                    <div className="groupMemberInfo">
-                    <h2>{member.username}</h2>
-                    <h1> {member.member_type}</h1>
-                    </div>
-
-                    <div className="groupMemberInfo">
-                    <h2>{member.email}</h2>
-                    <h1> {member.member_type}</h1>
-                    </div>
-                </div>
-            ))}
-            </div>
-            }
-                <h2>Event page</h2>
-                <MakeEvent slug={slug} />
-            {events&&
-              
-            <div className="container">
-
-            {events.map((event,index) => (
-                <div className="groupEvent" key={index}>
-                    
-                    <div className="groupPost">
-                    <div className="postti">
-                    <h2>{event.title}</h2>
-                    <p>{event.description}</p>
-                       
-                        <div className="contents">
-                    <p>{event.date}</p>
-                    <p>{event.event_date}</p>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            ))}
-            </div>
-            }
-            {joinRequest &&
-            <div className="container">
-
-            {joinRequest.map((request,index) => (
-                <div className="groupPost" key={index}>
-                    {request&&
-                    <div className="postti">
-                    <h2>{request.username}</h2>
-                    <h1>{request.email}</h1>
-                    </div>
-                    }
-                </div>
-            ))}
-            </div>
-            }
-
-            {userType === "creator" &&
-            <div className="container">
-                <h1>Join Requests</h1>
-                {joinRequest &&
-                <div>
-                    {joinRequest.map((request,index) => (
-                        <div className="groupJoinRequest" key={index}>
-                            {request&&
-                            <div className="groupJoinRequestInfo">
-                                <h2>{request.username}</h2>
-                                <h1>{request.email}</h1>
-                                </div>
-                            }
-                        </div>
-                    ))}
-                </div>
-                }
-            </div>
-            }
-    </div>
-        </>
-    )
-} */
-
 function MakeComment(props) {
   const post_id = props.post_id;
   const group_name = props.group_name;
@@ -942,4 +834,3 @@ function ToggleComments(props) {
   );
 }
 
-//export default GroupPage;
